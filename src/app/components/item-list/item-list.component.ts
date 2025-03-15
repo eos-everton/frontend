09c5cef829085dc95957396fab8b358a9c2371ad
eos-eveerton <everton.santos@educ.rec.br>
2025-03-14 23:35:48 -0300

    feat: create addItems, editItem and deleteItems

--- src/app/components/item-list/item-list.component.ts
+++ src/app/components/item-list/item-list.component.ts
@@ -3,16 +3,28 @@ import { Component, OnInit } from '@angular/core';
 import { ItemService } from '../../services/item.service';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { ItemFormComponent } from "../item-form/item-form.component";
+import { ItemFormEditComponent } from "../item-form-edit/item-form-edit.component";
 
 
 @Component({
   selector: 'app-item-list',
-  imports: [FormsModule, CommonModule],
+  imports: [
+    FormsModule,
+    CommonModule,
+    ItemFormComponent,
+    ItemFormEditComponent,
+  ],
   templateUrl: './item-list.component.html',
   styleUrls: ['./item-list.component.css'],
 })
 export class ItemListComponent implements OnInit {
   items: any[] = [];
+  id: string = '';
+  isItemsIn: boolean = false;
+  isEditItemsIn: boolean = false;
+  isTableIn: boolean = false;
+  selectedItem: any = null;
 
   constructor(private itemService: ItemService) {}
 
@@ -29,5 +41,37 @@ export class ItemListComponent implements OnInit {
         console.error('Erro ao carregar os itens', error);
       }
     );
+  }
+
+  deleteItems(id: number) {
+    if (confirm('Tem certeza que deseja deletar este item?')) {
+      this.itemService.deleteItem(id).subscribe({
+        next: () => {
+          this.items = this.items.filter((item) => item.id !== id);
+          alert('Item deletado com sucesso!');
+        },
+        error: (err) => alert('Erro ao deletar o item: ' + err.message),
+      });
+    }
+  }
+
+  onItemUpdated(updatedItem: any) {
+    const index = this.items.findIndex((i) => i.id === updatedItem.id);
+    if (index !== -1) this.items[index] = updatedItem;
+    this.selectedItem = null; // Esconde o formulário de edição
+  }
+
+  cancelEdit() {
+    this.selectedItem = null; // Esconde o formulário sem salvar
+  }
+
+  addItems() {
+    this.isItemsIn = true;
+    this.isTableIn = true;
+  }
+
+  editItem(item: any) {
+    console.log(item)
+    this.selectedItem = { ...item };
   }
 }

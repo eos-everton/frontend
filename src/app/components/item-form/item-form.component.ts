c8e4894ffc925c7d050e03d04ee1570777d00bf9
eos-eveerton <everton.santos@educ.rec.br>
2025-03-14 23:34:14 -0300

    feat: update for create function

--- src/app/components/item-form/item-form.component.ts
+++ src/app/components/item-form/item-form.component.ts
@@ -1,11 +1,30 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ItemService } from '../../services/item.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-item-form',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './item-form.component.html',
-  styleUrl: './item-form.component.scss'
+  styleUrl: './item-form.component.scss',
 })
 export class ItemFormComponent {
+  name: string = '';
+  description: string = '';
 
+  constructor(private itemService: ItemService, private router: Router) {}
+
+  registerItems() {
+    this.itemService.createItem(this.name, this.description).subscribe({
+      next: (res) => {
+        console.log('Cadastro bem-sucedido', res);
+        alert('Cadastro bem-sucedido');
+        this.router.navigate(['/items']).then(() => {
+          location.reload();
+        });
+      },
+      error: () => alert('error!'),
+    });
+  }
 }
